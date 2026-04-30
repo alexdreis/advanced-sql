36542a3c9844c2a2c37cc62702081bccd255c05c
Alex Dreis <alexander.dreis@mymail.nwtc.edu>
2026-04-30 05:11:02 -0500

    made some formatting changes for folders. uploaded week 5 assignments and my share of the group project work

---
+++ Week5-SSIS-Import/TableCreation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Week5_SSIS_Import 
+(
+	VarianceA decimal(15,10) not null,
+	CurrencyType varchar(5) not null,
+	TransDate smalldatetime not null,
+	VarianceB decimal(15,10) not null
+)
